5ac7c0359e284df5ac011127568849fecb0d6978
Ajju <ajinkyaawachar33@gmail.com>
2025-03-22 20:01:47 +0530

    portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar({menuOpen, setMenuOpen}) {
 
 
   return (
-    <nav className='fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg'>
+    <nav className='fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg '>
         <div className="max-w-5xl mx-auto px-4">
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className='font-mono text-xl font-bold text-white'>Ajinkya's<span className='text-blue-500'> Portfolio</span></a>
@@ -18,10 +18,10 @@ function Navbar({menuOpen, setMenuOpen}) {
                 </div>
 
                 <div className="hidden md:flex items-center space-x-8">
-                    <a href="#home" className="text-gray-300 hover:text-white transition-colors"> Home </a>
-                    <a href="#about" className="text-gray-300 hover:text-white transition-colors"> About </a>
-                    <a href="#projects" className="text-gray-300 hover:text-white transition-colors"> Projects </a>
-                    <a href="#contact" className="text-gray-300 hover:text-white transition-colors"> Contact </a>
+                    <a href="#home" className="text-gray-300 hover:text-white hover:scale-110 transition-colors"> Home </a>
+                    <a href="#about" className="text-gray-300 hover:text-white transition-colors hover:scale-110"> About </a>
+                    <a href="#projects" className="text-gray-300 hover:text-white transition-colors hover:scale-110"> Projects </a>
+                    <a href="#contact" className="text-gray-300 hover:text-white transition-colors hover:scale-110"> Contact </a>
                 </div>
             </div>
         </div>
